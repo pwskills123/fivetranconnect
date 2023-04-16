ff7bbab5757dbf5cd8bcead6a4a75fc54baaff1d
pwskills123 <md.a@pw.live>
2023-04-16 18:27:53 +0530

    newokokcodeREMOVE

--- models/test/sample.sql
+++ models/test/sample.sql
@@ -1,4 +1,3 @@
-SELECT COUNT(DISTINCT CUSTOMER_ID) AS total_customers, SUM(ORDER_TOTAL) AS total_revenue
-FROM fivetran_database.sql_server_rds_dbo.customers
-WHERE ORDER_DATE BETWEEN '2022-03-22' AND '2022-03-23'
+SELECT * FROM sql_server_rds_dbo.customers
+
 
