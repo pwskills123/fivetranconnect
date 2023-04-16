ebb82eabe57182399e2080ae5764b8c9916c04e0
pwskills123 <md.a@pw.live>
2023-04-16 18:19:33 +0530

    newokokcode

--- models/test/sample.sql
+++ models/test/sample.sql
@@ -1,3 +1,4 @@
 SELECT COUNT(DISTINCT customer_id) AS total_customers, SUM(order_total) AS total_revenue
-FROM FIVETRAN_DATABASE.SQL_SERVER_RDS_DBO.CUSTOMERS
+FROM fivetran_database.sql_server_rds_dbo.customers
 WHERE order_date BETWEEN '2022-03-22' AND '2022-03-23'
+
